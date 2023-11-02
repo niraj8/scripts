ffe5c61610fc47d9baebfa070ca243b2253372b6
Niraj <niraj@appcd.com>
2023-11-03 01:49:08 +0530

    echo repo branch in pull.sh

--- pull.sh
+++ pull.sh
@@ -4,6 +4,10 @@
 for dir in ~/Projects/**/*; do
     if [ -d "$dir" ]; then
         if [ -d "$dir/.git" ]; then
+            repo=$(basename $dir)
+            org=$(basename "$(dirname $dir)")
+            current_branch=$(git -C $dir branch --show-current)
+            echo "$org/$repo: $current_branch"
             git -C $dir pull --rebase
         fi
     fi
